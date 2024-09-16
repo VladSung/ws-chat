5f5d2f7708d350bc329d5b42e10e960113c57f82
Vlad <shutov.vladislav.2017@gmail.com>
2024-09-16 15:16:32 +0400

    edit css styles  and add css framework (sakura)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import 'sakura.css/css/normalize.css'
+import 'sakura.css/css/sakura-ink.css'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
